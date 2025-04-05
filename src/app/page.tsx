29261e2794324aaade40728eba3159b3c6909889
SHANbic <supagoof924@hotmail.com>
2025-04-05 09:59:16 +0200

    feat: use router instead of server redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return redirect("https://tinyurl.com/balades-moto");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://tinyurl.com/balades-moto");
+  });
+
+  return null;
 }
